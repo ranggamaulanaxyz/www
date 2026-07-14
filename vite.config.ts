be269663e1ac5d98553b3ae942c11894059228e7
Rangga Maulana <mr.ranggamaulana@gmail.com>
2026-07-14 21:15:50 +0700

    chore: migrate to Cloudflare Vite plugin and update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
+import { cloudflare } from "@cloudflare/vite-plugin";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter()],
+  plugins: [
+    cloudflare({ viteEnvironment: { name: "ssr" } }),
+    tailwindcss(),
+    reactRouter(),
+  ],
   resolve: {
     tsconfigPaths: true,
   },
